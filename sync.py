186342dbc56e9740a078ffa739c2cd24e3a6c80a
Evgenii Gurzhii <evgeniigurzhii@gmail.com>
2021-12-14 16:34:11 +0300

    Some cosmetic changes

--- sync.py
+++ sync.py
@@ -1,9 +1,10 @@
 import os
 import time
-from datetime import datetime
 import shutil
 import logging
 import argparse
+import filecmp
+from datetime import datetime
 
 
 def synchronizing_folders(src_path, dest_path, logs_path, interval):
@@ -13,21 +14,21 @@ def synchronizing_folders(src_path, dest_path, logs_path, interval):
         :type src_path: string
         :param dest_path: Destination folder path, will be created if not exists
         :type dest_path: string
-        :param src_path: Path to folder to store logs, will be created if not exists
-        :type src_path: string
+        :param logs_path: Path to folder to store logs, will be created if not exists
+        :type logs_path: string
         :param interval: Synchronisation interval in seconds, default is 30 seconds
         :type interval: int
         :return: None
     """
-    #configuring logging
-    log_filename = f"{logs_path}{os.sep}{datetime.now().strftime('%Y_%m_%d-%H_%M_%S_%f')}"
+    # configuring logging
+    log_file = f"{logs_path}{os.sep}{datetime.now().strftime('%Y_%m_%d-%H_%M_%S_%f')}"
     if not os.path.exists(logs_path):
         print(f"Logs directory {logs_path} doesn't exist, creating..")
-    os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
     logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s",
-                        handlers=[logging.FileHandler(log_filename), logging.StreamHandler()])
+                        handlers=[logging.FileHandler(log_file), logging.StreamHandler()])
 
-    #checking paths input
+    # checking paths input
     if not os.path.exists(src_path):
         logging.error(f"Source directory {src_path} doesn't exist!")
         exit()
@@ -35,41 +36,38 @@ def synchronizing_folders(src_path, dest_path, logs_path, interval):
         logging.info(f"Destination directory {dest_path} doesn't exist, creating..")
         os.makedirs(os.path.dirname(dest_path + os.sep), exist_ok=True)
 
-    #starting sync
-    logging.info(f"Starting sync with interval of {interval} seconds")
+    # starting sync
+    logging.info(f"Starting synchronization with interval of {interval} seconds")
     while True:
         start_time = time.time()
-        for current_folder, sub_folders, current_files in os.walk(src_path):
-
-            #handling folders
+        for current_folder, current_sub_folders, current_files in os.walk(src_path):
+            # handling folders
             dest_sub_folders = next(os.walk(current_folder.replace(src_path, dest_path)))[1]
-            folders_to_remove = set(dest_sub_folders) - set(sub_folders)
-            folders_to_create = set(sub_folders) - set(dest_sub_folders)
+            folders_to_remove = set(dest_sub_folders) - set(current_sub_folders)
+            folders_to_create = set(current_sub_folders) - set(dest_sub_folders)
             for folder in folders_to_remove:
-                dest_folder_path = f"{current_folder}{os.sep}{folder}".replace(src_path, dest_path)
-                logging.info(f"Removing folder {dest_folder_path}")
-                shutil.rmtree(dest_folder_path)
+                dest_folder = f"{current_folder}{os.sep}{folder}".replace(src_path, dest_path)
+                logging.info(f"Removing folder {dest_folder}")
+                shutil.rmtree(dest_folder)
             for folder in folders_to_create:
-                dest_folder_path = f"{current_folder}{os.sep}{folder}".replace(src_path, dest_path)
-                logging.info(f"Creating folder {dest_folder_path}")
-                os.mkdir(dest_folder_path)
-
-            #handling files
+                dest_folder = f"{current_folder}{os.sep}{folder}".replace(src_path, dest_path)
+                logging.info(f"Creating folder {dest_folder}")
+                os.mkdir(dest_folder)
+            # handling files
             dest_files = next(os.walk(current_folder.replace(src_path, dest_path)))[2]
             files_to_remove = set(dest_files) - set(current_files)
             for file in files_to_remove:
-                dest_file_path = f"{current_folder}{os.sep}{file}".replace(src_path, dest_path)
-                logging.info(f"Removing file {dest_file_path}")
-                os.remove(dest_file_path)
+                dest_file = f"{current_folder}{os.sep}{file}".replace(src_path, dest_path)
+                logging.info(f"Removing file {dest_file}")
+                os.remove(dest_file)
             for file in current_files:
-                src_file_path = f"{current_folder}{os.sep}{file}"
-                dest_file_path = src_file_path.replace(src_path, dest_path)
-                if os.path.isfile(dest_file_path) and os.stat(src_file_path).st_mtime == os.stat(dest_file_path).st_mtime:
+                src_file = f"{current_folder}{os.sep}{file}"
+                dest_file = src_file.replace(src_path, dest_path)
+                if os.path.isfile(dest_file) and filecmp.cmp(src_file, dest_file):
                     continue
-                logging.info(f"Copying file {src_file_path}")
-                shutil.copy2(src_file_path, dest_file_path)
-
-        #handling sleep according to the passed time
+                logging.info(f"Copying file {src_file}")
+                shutil.copy2(src_file, dest_file)
+        # handling sleep according to the passed time
         if (time.time() - start_time) // interval >= 1:
             time.sleep(interval)
         else:
@@ -78,7 +76,7 @@ def synchronizing_folders(src_path, dest_path, logs_path, interval):
 
 if __name__ == "__main__":
 
-    #parsing arguments
+    # parsing arguments
     parser = argparse.ArgumentParser(description='Sync script')
     parser.add_argument('-s', dest="src", type=str)
     parser.add_argument('-d', dest="dst", type=str)
@@ -86,7 +84,7 @@ if __name__ == "__main__":
     parser.add_argument('-l', dest="logs", type=str)
     args = parser.parse_args()
 
-    #stripping last separator if there is the case
+    # stripping last separator if there is the case
     if args.src[-1] == "\\" or "/" : args.src = args.src[:-1] 
     if args.dst[-1] == "\\" or "/" : args.dst = args.dst[:-1] 
     if args.logs[-1] == "\\" or "/" : args.logs = args.logs[:-1]
